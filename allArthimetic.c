bf11070bdc6c356e88cebab8b963f5d44ca2afcb
kelcheone <80192475+kelcheone@users.noreply.github.com>
2021-03-07 10:45:19 +0300

    Add files via upload

---
+++ allArthimetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+
+    int num1, num2, res;
+    printf("Enter  any two numbers: \n");
+    scanf("%d%d", &num1, &num2);
+    res = num1 +num2;
+    printf("\nAddition = %d", res);
+    res = num1- num2;
+    printf("\nSubtraction = %d", res);
+    res = num1*num2;
+    printf("\nMultitplication = %d", res);
+    res = num1/num2;
+    printf("\nDivision = %d,", res);
+    getch();
+    return (1);
+
+}
